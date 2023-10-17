90cf76e9c4f1f2f671648e9fea8563f73bc65273
Felipe Buso <felipe.buso@hotmail.com>
2023-10-17 11:00:16 -0300

    feat-utils-read-file

--- orm_sqlalchemy/index.py
+++ orm_sqlalchemy/index.py
@@ -2,21 +2,41 @@ from sqlalchemy import create_engine, text
 
 
 class DatabaseHandler:
+    """
+    Esta classe fornece métodos para conectar a um banco de dados PostgreSQL usando SQLAlchemy e executar consultas.
+    """
+
     def __init__(self):
         # Crie a URL de conexão no construtor
         db_url = f"postgresql://postgres:postgres@localhost:5432/airbnb"
         self.engine = create_engine(db_url)
 
     def connect(self):
-        # Abra uma conexão com o banco de dados
+        """
+        Abre uma conexão com o banco de dados.
+        """
         self.connection = self.engine.connect()
 
     def disconnect(self):
-        # Feche a conexão com o banco de dados
+        """
+        Fecha a conexão com o banco de dados.
+        """
         self.connection.close()
 
-    def execute_query(self, query_string):
-        # Execute uma consulta SQL e retorne os resultados
-        query = text(query_string)
-        result = self.connection.execute(query)
-        return result.fetchall()
+    def execute_query(self, query_string) -> list:
+        """
+        Executa uma consulta SQL e retorna os resultados.
+
+        Args:
+            query_string (str): A consulta SQL a ser executada.
+
+        Returns:
+            list: Uma lista de resultados da consulta | None.
+        """
+        try:
+            query = text(query_string)
+            result = self.connection.execute(query)
+            return result.fetchall()
+        except Exception as error:
+            print(error)
+            return None
