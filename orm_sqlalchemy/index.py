6a1e1d26cc8f1cda20c34b154f96f816f40653ed
Felipe Buso <felipe.buso@hotmail.com>
2023-10-17 11:52:20 -0300

    feat-sqlalchemy-models-listining-reviews

--- orm_sqlalchemy/index.py
+++ orm_sqlalchemy/index.py
@@ -1,4 +1,54 @@
-from sqlalchemy import create_engine, text
+from typing import List
+from typing import Optional
+from sqlalchemy import ForeignKey
+from sqlalchemy import String
+from sqlalchemy import create_engine
+from sqlalchemy import text
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import Session
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import relationship
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class Listining(Base):
+    __tablename__ = "listining"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    listining_id: Mapped[int]
+    listing_url: Mapped[str]
+    scrape_id: Mapped[int]
+    host_id: Mapped[int]
+    host_listings_count: Mapped[int]
+    reviews: Mapped[List["Reviews"]] = relationship(
+        back_populates="listining", cascade="all, delete-orphan"
+    )
+
+    def __repr__(self) -> str:
+        return f"listining(id={self.id!r}, listining_id={self.listining_id!r}, listing_url={self.listing_url!r})"
+
+
+class Reviews(Base):
+    __tablename__ = "reviews"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    listing_id: Mapped[int] = mapped_column(ForeignKey("listining.listining_id"))
+    review_id: Mapped[int]
+    date: Mapped[str]
+    reviewer_id: Mapped[int]
+    reviewer_name: Mapped[str]
+    comments: Mapped[Optional[str]]
+
+    listining: Mapped["Listining"] = relationship(back_populates="reviews")
+
+    def __repr__(self) -> str:
+        return (
+            f"Reviews(id={self.id!r}, review_id={self.review_id!r}, date={self.date!r})"
+        )
 
 
 class DatabaseHandler:
@@ -23,6 +73,12 @@ class DatabaseHandler:
         """
         self.connection.close()
 
+    def create_tables(self):
+        """
+        Cria as tabelas no banco de dados com base classes dos recursos
+        """
+        Base.metadata.create_all(self.engine)
+
     def execute_query(self, query_string) -> list:
         """
         Executa uma consulta SQL e retorna os resultados.
@@ -40,3 +96,39 @@ class DatabaseHandler:
         except Exception as error:
             print(error)
             return None
+
+    def post_listining(self, data: List[dict]):
+        """
+        Grava os dados na tabela Lisiting.
+
+        Args:
+            data (list[dict]): Lista de dicionários com os dados.
+
+        """
+        with Session(self.engine) as session:
+            list_listining = list()
+            for payload in data:
+                list_listining.append(Listining(**payload))
+            try:
+                session.add_all(list_listining)
+                session.commit()
+            except Exception as error:
+                print(error)
+
+    def post_reviews(self, data: List[dict]):
+        """
+        Grava os dados na tabela Reviews.
+
+        Args:
+            data (list[dict]): Lista de dicionários com os dados.
+
+        """
+        with Session(self.engine) as session:
+            list_reviews = list()
+            for payload in data:
+                list_reviews.append(Reviews(**payload))
+            try:
+                session.add_all(list_reviews)
+                session.commit()
+            except Exception as error:
+                print(error)
