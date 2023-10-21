5849b4f16d8d398b6e220f363e5285193e0fffc9
Felipe Buso <felipe.buso@hotmail.com>
2023-10-21 10:21:42 -0300

    feat-utils

---
+++ utils/utils.py
@@ -0,0 +1,15 @@
+import re
+
+
+def only_number(value: str) -> float:
+    """
+    Remove todos os caracteres menos dígitos e retorna um flat
+
+    Args:
+        value (str): Valor em formato string
+
+    Returns:
+        float
+    """
+    result = re.sub(r"[^0-9.]", "", value)
+    return float(result)
