81cc7365fa65b580d06d080a9595db21eb1e7bc7
Felipe Buso <felipe.buso@hotmail.com>
2023-10-24 22:13:33 -0300

    refactor-model-count_by_neighbourhood

--- AdaTech/models/count_by_neighbourhood.sql
+++ AdaTech/models/count_by_neighbourhood.sql
@@ -3,7 +3,17 @@
 WITH neighbourhood_counts AS (
         SELECT
             host_neighbourhood,
-            COUNT(*) AS neighbourhood_count
+            COUNT(*) AS neighbourhood_count,
+            AVG(
+                CAST(
+                    REPLACE(
+                            REPLACE
+(price, '$', ''),
+                                ',',
+                                ''
+                        ) AS numeric
+                )
+            ) AS avg_price
         FROM listings
         GROUP BY
             host_neighbourhood
