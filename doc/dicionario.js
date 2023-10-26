2ae7b7522fad1b04330b77599bdc7658ce7a2a1a
Luis Felipe Gilisk de Oliveira <95372771+felipegilisk@users.noreply.github.com>
2023-10-25 22:15:07 -0300

    feature: 1st layer of [trusted] (#4)
    Co-authored-by: Felipe Buso <felipe.buso@hotmail.com>

---
+++ doc/dicionario.js
@@ -0,0 +1,602 @@
+listings = [
+    {
+        "num": "01",
+        "column_name": "id",
+        "dictionary": "Identificador da AirBnB",
+        "calculated": "Não",
+        "data_type": "int",
+        "data_type_2": "int"
+    },
+    {
+        "num": "02",
+        "column_name": "listing_url",
+        "dictionary": "URL do imóvel",
+        "calculated": "Sim",
+        "data_type": "str",
+        "data_type_2": "str"
+    },
+    {
+        "num": "03",
+        "column_name": "scrape_id",
+        "dictionary": "Identificador do scraping",
+        "calculated": "Sim",
+        "data_type": "BigInteger",
+        "data_type_2": "BigInteger"
+    },
+    {
+        "num": "04",
+        "column_name": "last_scraped",
+        "dictionary": "Data do último scrapping",
+        "calculated": "Sim",
+        "data_type": "str",
+        "data_type_2": "datetime"
+    },
+    {
+        "num": "05",
+        "column_name": "source",
+        "dictionary": "Fonte de coleta do scraping",
+        "calculated": "Não",
+        "data_type": "str",
+        "data_type_2": "str"
+    },
+    {
+        "num": "06",
+        "column_name": "name",
+        "dictionary": "Título do imóvel",
+        "calculated": "Não",
+        "data_type": "str",
+        "data_type_2": "str"
+    },
+    {
+        "num": "07",
+        "column_name": "description",
+        "dictionary": "Descrição geral do imóvel",
+        "calculated": "Não",
+        "data_type": "str",
+        "data_type_2": "str"
+    },
+    {
+        "num": "08",
+        "column_name": "neighborhood_overview",
+        "dictionary": "Descrição da vizinhança fornecida pelo anfitrião",
+        "calculated": "Não",
+        "data_type": "str",
+        "data_type_2": "str"
+    },
+    {
+        "num": "09",
+        "column_name": "picture_url",
+        "dictionary": "URL da imagem do imóvel",
+        "calculated": "Não",
+        "data_type": "str",
+        "data_type_2": "str"
+    },
+    {
+        "num": "10",
+        "column_name": "host_id",
+        "dictionary": "Identificador do anfitrião",
+        "calculated": "Não",
+        "data_type": "int",
+        "data_type_2": "int"
+    },
+    {
+        "num": "11",
+        "column_name": "host_url",
+        "dictionary": "URL do anfitrião no AirBnB",
+        "calculated": "Sim",
+        "data_type": "str",
+        "data_type_2": "str"
+    },
+    {
+        "num": "12",
+        "column_name": "host_name",
+        "dictionary": "Nome do anfitrião",
+        "calculated": "Não",
+        "data_type": "str",
+        "data_type_2": "str"
+    },
+    {
+        "num": "13",
+        "column_name": "host_since",
+        "dictionary": "Data de cadastro do anfitrião (como usuário AirBnB)",
+        "calculated": "Não",
+        "data_type": "str",
+        "data_type_2": "str"
+    },
+    {
+        "num": "14",
+        "column_name": "host_location",
+        "dictionary": "Localização do anfitrião",
+        "calculated": "Não",
+        "data_type": "str",
+        "data_type_2": "str"
+    },
+    {
+        "num": "15",
+        "column_name": "host_about",
+        "dictionary": "Descrição do anfitrião",
+        "calculated": "Não",
+        "data_type": "str",
+        "data_type_2": "str"
+    },
+    {
+        "num": "16",
+        "column_name": "host_response_time",
+        "dictionary": "Tempo de resposta do anfitrião",
+        "calculated": "Não",
+        "data_type": "str",
+        "data_type_2": "str"
+    },
+    {
+        "num": "17",
+        "column_name": "host_response_rate",
+        "dictionary": "Índice de resposta do anfitrião",
+        "calculated": "Não",
+        "data_type": "str",
+        "data_type_2": "str"
+    },
+    {
+        "num": "18",
+        "column_name": "host_acceptance_rate",
+        "dictionary": "Índice de aceitação de reservas do anfitrião",
+        "calculated": "Não",
+        "data_type": "str",
+        "data_type_2": "float"
+    },
+    {
+        "num": "19",
+        "column_name": "host_is_superhost",
+        "dictionary": "Flag [Superhost] do AirBnB",
+        "calculated": "Não",
+        "data_type": "bool",
+        "data_type_2": "bool"
+    },
+    {
+        "num": "20",
+        "column_name": "host_thumbnail_url",
+        "dictionary": "URL do avatar do anfitrião (miniatura)",
+        "calculated": "Não",
+        "data_type": "str",
+        "data_type_2": "str"
+    },
+    {
+        "num": "21",
+        "column_name": "host_picture_url",
+        "dictionary": "URL do avatar do anfitrião",
+        "calculated": "Não",
+        "data_type": "str",
+        "data_type_2": "str"
+    },
+    {
+        "num": "22",
+        "column_name": "host_neighbourhood",
+        "dictionary": "Vizinhança do anfitrião",
+        "calculated": "Não",
+        "data_type": "str",
+        "data_type_2": "str"
+    },
+    {
+        "num": "23",
+        "column_name": "host_listings_count",
+        "dictionary": "Número de imóveis que o anfitrião possui",
+        "calculated": "Não",
+        "data_type": "float",
+        "data_type_2": "float"
+    },
+    {
+        "num": "24",
+        "column_name": "host_total_listings_count",
+        "dictionary": "Número de imóveis que o anfitrião possui",
+        "calculated": "Não",
+        "data_type": "float",
+        "data_type_2": "float"
+    },
+    {
+        "num": "25",
+        "column_name": "host_verifications",
+        "dictionary": "Dados confirmados do anfitrião",
+        "calculated": "Não",
+        "data_type": "str",
+        "data_type_2": "str"
+    },
+    {
+        "num": "26",
+        "column_name": "host_has_profile_pic",
+        "dictionary": "Anfitrião possui foto de perfil",
+        "calculated": "Não",
+        "data_type": "bool",
+        "data_type_2": "bool"
+    },
+    {
+        "num": "27",
+        "column_name": "host_identity_verified",
+        "dictionary": "Identidade do anfitrião verificada",
+        "calculated": "Não",
+        "data_type": "bool",
+        "data_type_2": "bool"
+    },
+    {
+        "num": "28",
+        "column_name": "neighbourhood",
+        "dictionary": "Vizinhança do imóvel",
+        "calculated": "Não",
+        "data_type": "str",
+        "data_type_2": "str"
+    },
+    {
+        "num": "29",
+        "column_name": "neighbourhood_cleansed",
+        "dictionary": "[Vizinhança] confirmada por geolocalização",
+        "calculated": "Sim",
+        "data_type": "str",
+        "data_type_2": "str"
+    },
+    {
+        "num": "30",
+        "column_name": "neighbourhood_group_cleansed",
+        "dictionary": "[Vizinhança] confirmada por geolocalização",
+        "calculated": "Sim",
+        "data_type": "float",
+        "data_type_2": "-- removida --"
+    },
+    {
+        "num": "31",
+        "column_name": "latitude",
+        "dictionary": "Coordenada de geolocalização",
+        "calculated": "Não",
+        "data_type": "float",
+        "data_type_2": "float"
+    },
+    {
+        "num": "32",
+        "column_name": "longitude",
+        "dictionary": "Coordenada de geolocalização",
+        "calculated": "Não",
+        "data_type": "float",
+        "data_type_2": "float"
+    },
+    {
+        "num": "33",
+        "column_name": "property_type",
+        "dictionary": "Tipo de propriedade do imóvel",
+        "calculated": "Não",
+        "data_type": "str",
+        "data_type_2": "str"
+    },
+    {
+        "num": "34",
+        "column_name": "room_type",
+        "dictionary": "Tipo de acomodação",
+        "calculated": "Não",
+        "data_type": "str",
+        "data_type_2": "str"
+    },
+    {
+        "num": "35",
+        "column_name": "accommodates",
+        "dictionary": "Número máximo de pessoas hospedadas",
+        "calculated": "Não",
+        "data_type": "int",
+        "data_type_2": "int"
+    },
+    {
+        "num": "36",
+        "column_name": "bathrooms",
+        "dictionary": "Quantidade de banheiros",
+        "calculated": "Não",
+        "data_type": "float",
+        "data_type_2": "-- removida --"
+    },
+    {
+        "num": "37",
+        "column_name": "bathrooms_text",
+        "dictionary": "Quantidade de banheiros",
+        "calculated": "Não",
+        "data_type": "str",
+        "data_type_2": "str"
+    },
+    {
+        "num": "38",
+        "column_name": "bedrooms",
+        "dictionary": "Quantidade de quartos",
+        "calculated": "Não",
+        "data_type": "float",
+        "data_type_2": "float"
+    },
+    {
+        "num": "39",
+        "column_name": "beds",
+        "dictionary": "Quantidade de camas",
+        "calculated": "Não",
+        "data_type": "float",
+        "data_type_2": "int"
+    },
+    {
+        "num": "40",
+        "column_name": "amenities",
+        "dictionary": "Facilidades",
+        "calculated": "Não",
+        "data_type": "str",
+        "data_type_2": "str"
+    },
+    {
+        "num": "41",
+        "column_name": "price",
+        "dictionary": "Calor da diária (moeda local)",
+        "calculated": "Não",
+        "data_type": "str",
+        "data_type_2": "str"
+    },
+    {
+        "num": "42",
+        "column_name": "minimum_nights",
+        "dictionary": "Quantidade mínima de diárias",
+        "calculated": "Não",
+        "data_type": "int",
+        "data_type_2": "int"
+    },
+    {
+        "num": "43",
+        "column_name": "maximum_nights",
+        "dictionary": "Quantidade máxima de diárias",
+        "calculated": "Não",
+        "data_type": "int",
+        "data_type_2": "int"
+    },
+    {
+        "num": "44",
+        "column_name": "minimum_minimum_nights",
+        "dictionary": "Menor valor registrado da [Quantidade mínima de diária] registrada no calendário",
+        "calculated": "Sim",
+        "data_type": "int",
+        "data_type_2": "int"
+    },
+    {
+        "num": "45",
+        "column_name": "maximum_minimum_nights",
+        "dictionary": "Maior valor registrado da [Quantidade mínima de diária] registrada no calendário",
+        "calculated": "Sim",
+        "data_type": "int",
+        "data_type_2": "int"
+    },
+    {
+        "num": "46",
+        "column_name": "minimum_maximum_nights",
+        "dictionary": "Menor valor registrado da [Quantidade máxima de diária] registrada no calendário",
+        "calculated": "Sim",
+        "data_type": "int",
+        "data_type_2": "int"
+    },
+    {
+        "num": "47",
+        "column_name": "maximum_maximum_nights",
+        "dictionary": "Maior valor registrado da [Quantidade máxima de diária] registrada no calendário",
+        "calculated": "Sim",
+        "data_type": "int",
+        "data_type_2": "int"
+    },
+    {
+        "num": "48",
+        "column_name": "minimum_nights_avg_ntm",
+        "dictionary": "Valor médio registrado da [Quantidade mínima de diária] registrada no calendário",
+        "calculated": "Sim",
+        "data_type": "float",
+        "data_type_2": "float"
+    },
+    {
+        "num": "49",
+        "column_name": "maximum_nights_avg_ntm",
+        "dictionary": "Valor médio registrado da [Quantidade máxima de diária] registrada no calendário",
+        "calculated": "Sim",
+        "data_type": "float",
+        "data_type_2": "float"
+    },
+    {
+        "num": "50",
+        "column_name": "calendar_updated",
+        "dictionary": "Data de atualização do calendário",
+        "calculated": "Não",
+        "data_type": "float",
+        "data_type_2": "-- removida --"
+    },
+    {
+        "num": "51",
+        "column_name": "has_availability",
+        "dictionary": "Possui disponibilidade",
+        "calculated": "Não",
+        "data_type": "bool",
+        "data_type_2": "bool"
+    },
+    {
+        "num": "52",
+        "column_name": "availability_30",
+        "dictionary": "Disponibilidade nos próximos 30 dias",
+        "calculated": "Sim",
+        "data_type": "int",
+        "data_type_2": "int"
+    },
+    {
+        "num": "53",
+        "column_name": "availability_60",
+        "dictionary": "Disponibilidade nos próximos 60 dias",
+        "calculated": "Sim",
+        "data_type": "int",
+        "data_type_2": "int"
+    },
+    {
+        "num": "54",
+        "column_name": "availability_90",
+        "dictionary": "Disponibilidade nos próximos 90 dias",
+        "calculated": "Sim",
+        "data_type": "int",
+        "data_type_2": "int"
+    },
+    {
+        "num": "55",
+        "column_name": "availability_365",
+        "dictionary": "Disponibilidade nos próximos 365 dias",
+        "calculated": "Sim",
+        "data_type": "int",
+        "data_type_2": "int"
+    },
+    {
+        "num": "56",
+        "column_name": "calendar_last_scraped",
+        "dictionary": "Data de scraping do calendário",
+        "calculated": "Não",
+        "data_type": "str",
+        "data_type_2": "datetime"
+    },
+    {
+        "num": "57",
+        "column_name": "number_of_reviews",
+        "dictionary": "Número de avaliações",
+        "calculated": "Não",
+        "data_type": "int",
+        "data_type_2": "int"
+    },
+    {
+        "num": "58",
+        "column_name": "number_of_reviews_ltm",
+        "dictionary": "Número de avaliações nos últimos 12 meses",
+        "calculated": "Sim",
+        "data_type": "int",
+        "data_type_2": "int"
+    },
+    {
+        "num": "59",
+        "column_name": "number_of_reviews_l30d",
+        "dictionary": "Número de avaliações nos últimos 30 dias",
+        "calculated": "Sim",
+        "data_type": "int",
+        "data_type_2": "int"
+    },
+    {
+        "num": "60",
+        "column_name": "first_review",
+        "dictionary": "Data da primeira avaliação",
+        "calculated": "Sim",
+        "data_type": "str",
+        "data_type_2": "datetime"
+    },
+    {
+        "num": "61",
+        "column_name": "last_review",
+        "dictionary": "Data de última avaliação",
+        "calculated": "Sim",
+        "data_type": "str",
+        "data_type_2": "datetime"
+    },
+    {
+        "num": "62",
+        "column_name": "review_scores_rating",
+        "dictionary": "-",
+        "calculated": "Não",
+        "data_type": "float",
+        "data_type_2": "float"
+    },
+    {
+        "num": "63",
+        "column_name": "review_scores_accuracy",
+        "dictionary": "-",
+        "calculated": "Não",
+        "data_type": "float",
+        "data_type_2": "float"
+    },
+    {
+        "num": "64",
+        "column_name": "review_scores_cleanliness",
+        "dictionary": "-",
+        "calculated": "Não",
+        "data_type": "float",
+        "data_type_2": "float"
+    },
+    {
+        "num": "65",
+        "column_name": "review_scores_checkin",
+        "dictionary": "-",
+        "calculated": "Não",
+        "data_type": "float",
+        "data_type_2": "float"
+    },
+    {
+        "num": "66",
+        "column_name": "review_scores_communication",
+        "dictionary": "-",
+        "calculated": "Não",
+        "data_type": "float",
+        "data_type_2": "float"
+    },
+    {
+        "num": "67",
+        "column_name": "review_scores_location",
+        "dictionary": "-",
+        "calculated": "Não",
+        "data_type": "float",
+        "data_type_2": "float"
+    },
+    {
+        "num": "68",
+        "column_name": "review_scores_value",
+        "dictionary": "-",
+        "calculated": "Não",
+        "data_type": "float",
+        "data_type_2": "float"
+    },
+    {
+        "num": "69",
+        "column_name": "license",
+        "dictionary": "Número de permissão/registro/licença",
+        "calculated": "Não",
+        "data_type": "float",
+        "data_type_2": "-- removida --"
+    },
+    {
+        "num": "70",
+        "column_name": "instant_bookable",
+        "dictionary": "Agendamento instantâneo e sem validação do anfitrião",
+        "calculated": "Não",
+        "data_type": "bool",
+        "data_type_2": "bool"
+    },
+    {
+        "num": "71",
+        "column_name": "calculated_host_listings_count",
+        "dictionary": "Quantidade de imóveis que o anfitrião possui de acordo com o scraping desta cidade/região",
+        "calculated": "Sim",
+        "data_type": "int",
+        "data_type_2": "int"
+    },
+    {
+        "num": "72",
+        "column_name": "calculated_host_listings_count_entire_homes",
+        "dictionary": "Quantidade de imóveis completos (casas/apartamentos inteiros) que o anfitrião possui de acordo com o scraping desta cidade/região",
+        "calculated": "Sim",
+        "data_type": "int",
+        "data_type_2": "int"
+    },
+    {
+        "num": "73",
+        "column_name": "calculated_host_listings_count_private_rooms",
+        "dictionary": "Quantidade de quartos privativos que o anfitrião possui de acordo com o scraping desta cidade/região",
+        "calculated": "Sim",
+        "data_type": "int",
+        "data_type_2": "int"
+    },
+    {
+        "num": "74",
+        "column_name": "calculated_host_listings_count_shared_rooms",
+        "dictionary": "Quantidade de quartos compartilhados que o anfitrião possui de acordo com o scraping desta cidade/região",
+        "calculated": "Sim",
+        "data_type": "int",
+        "data_type_2": "int"
+    },
+    {
+        "num": "75",
+        "column_name": "reviews_per_month",
+        "dictionary": "Quantidade de avaliações que este imóvel tem desde o início do registro destes dados",
+        "calculated": "Sim",
+        "data_type": "float",
+        "data_type_2": "float"
+    }
+]
